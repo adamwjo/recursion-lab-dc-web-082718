961ae84620efb922441f412b00e27137e8ed8e3c
github username <github email address>
2019-01-10 14:08:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // Code your solution here!
 
+
+
+
+function printString(string) {
+    console.log(string[0])
+    if (string.length > 1) {
+        let newWord = string.substring(1)
+        printString(newWord)
+    }else {
+        return true 
+    }
+}
+
+function reverseString(str) {
+    if (str === "") {
+        return ""
+    }else {
+        return reverseString(str.substr(1)) + str.charAt(0)
+    }
+}
+    
